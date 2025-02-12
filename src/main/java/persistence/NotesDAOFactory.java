ae08c2e77b048f59debebbec9b8614bcc017cbcb
mirabellalorenzo <mirabellalorenzo03@gmail.com>
2025-02-12 10:17:17 +0100

    Resolved 2 sonarcloud issues in Styles.css and NotesDAOFactory

--- src/main/java/persistence/NotesDAOFactory.java
+++ src/main/java/persistence/NotesDAOFactory.java
@@ -3,6 +3,11 @@ package persistence;
 import config.AppConfig;
 
 public class NotesDAOFactory {
+
+    private NotesDAOFactory() {
+        throw new UnsupportedOperationException("Utility class - instantiation not allowed");
+    }
+
     public static NotesDAO getNotesDAO() {
         if (AppConfig.getStorageMode() == AppConfig.StorageMode.DATABASE) {
             return new FirebaseNotesDAO();
